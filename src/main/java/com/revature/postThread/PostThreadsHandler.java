7c28715c42362724b47e26cbb6c3430bb18021d9
metteec2 <chmettee@gmail.com>
2021-09-24 14:42:35 -0400

    Sean Smith created PostThreadsHandler class

--- src/main/java/com/revature/postThread/PostThreadsHandler.java
+++ src/main/java/com/revature/postThread/PostThreadsHandler.java
@@ -1,5 +1,35 @@
 package com.revature.postThread;
 
-public class PostThreadsHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class PostThreadsHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private static final Gson mapper = new GsonBuilder().setPrettyPrinting().create();
+    private final ThreadsRepo threadsRepo = new ThreadsRepo();
+
+    /**
+     * @param requestEvent
+     * @param context
+     * @return
+     * @Author - Sean Smith
+     */
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
+
+        LambdaLogger logger = context.getLogger();
+        logger.log("RECEIVED EVENT: " + requestEvent);
+
+        Threads threads = mapper.fromJson(requestEvent.getBody(), Threads.class);
+
+        threadsRepo.addThreads(threads);
+
+        return new APIGatewayProxyResponseEvent();
+    }
 
 }
