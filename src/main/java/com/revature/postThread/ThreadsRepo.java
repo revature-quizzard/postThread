f0a88667382b643df04a4ab426d759ec274e059d
metteec2 <chmettee@gmail.com>
2021-09-24 14:42:03 -0400

    Created threadsRepo class

--- src/main/java/com/revature/postThread/ThreadsRepo.java
+++ src/main/java/com/revature/postThread/ThreadsRepo.java
@@ -1,4 +1,28 @@
 package com.revature.postThread;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+
 public class ThreadsRepo {
+
+    private final DynamoDBMapper dbReader;
+
+    /**
+     * Constructor for initializing dbReader;
+     * @author - Charles Mettee
+     */
+    public ThreadsRepo() {
+        dbReader = new DynamoDBMapper(AmazonDynamoDBClientBuilder.defaultClient());
+    }
+
+    /**
+     * @param threads - the Threads object being posted to the database
+     * @author - Charles Mettee
+     */
+    public void addThreads(Threads threads){
+        dbReader.save(threads);
+    }
+
+
+
 }
