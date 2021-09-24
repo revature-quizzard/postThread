5e1afea16e74ff23afddf4b7e5763d226bc71f0b
metteec2 <chmettee@gmail.com>
2021-09-24 17:52:39 -0400

    Added testing suite for ThreadsService; no tests yet
    Signed-off-by: metteec2 <chmettee@gmail.com>

--- src/test/java/com/revature/postThread/services/ThreadsServiceTestSuite.java
+++ src/test/java/com/revature/postThread/services/ThreadsServiceTestSuite.java
@@ -1,2 +1,12 @@
-package com.revature.postThread.services;public class ThreadsServiceTestSuite {
+package com.revature.postThread.services;
+import com.revature.postThread.repositories.ThreadsRepo;
+
+import org.junit.*;
+import static org.mockito.Mockito.*;
+
+
+public class ThreadsServiceTestSuite {
+
+    ThreadsService sut;
+
 }
